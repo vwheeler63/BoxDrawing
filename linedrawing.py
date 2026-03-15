537cee0b4f8160ba882838514780ba9912d93319
Victor Wheeler <vaw_info998@comcast.net>
2026-03-15 11:20:42 -0600

    Initial Commit

---
+++ linedrawing.py
@@ -0,0 +1,16 @@
+""" ***********************************************************************
+Line Drawing
+
+Line Drawing is Sublime Text package enabling the user to temporarily
+enter "Line-Drawing Mode" and thereafter use Shift or Ctrl-Shift
+combined with arrow keys to draw lines in the text file being edited.
+Line characters are placed where the caret is made to travel with the
+arrow keys.
+
+Because Sublime Text does not allow the caret to travel to the right
+of the end of text lines, spaces are appended to lines to allow the
+caret to travel (and lines to be drawn) where the user indicates with
+the arrow keys.
+
+
+*********************************************************************** """
